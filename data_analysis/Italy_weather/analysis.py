97e13fb75ffe8ccd6eef775a10c335d2f12743f1
zonghuixu <xvzonghui@gmail.com>
2019-09-18 10:00:48 +0800

    pandas data analysis

--- data_analysis/Italy_weather/analysis.py
+++ data_analysis/Italy_weather/analysis.py
@@ -32,7 +32,8 @@ x5 = df_asti['day']
 y6 = df_torino['temp']
 x6 = df_torino['day']
 
-print(df_ravenna)
+fig, ax = plt.subplots()
+
 dist = [df_ravenna['dist'][0],
     df_cesena['dist'][0],
     df_faenza['dist'][0],
@@ -69,34 +70,32 @@ temp_min = [df_ravenna['temp'].min(),
     df_milano['temp'].min(),
     df_asti['temp'].min(),
     df_torino['temp'].min()
-]
-from sklearn.svm import SVR
+ ]
 
-# dist1是靠近海的城市集合，dist2是远离海洋的城市集合
 dist1 = dist[0:5]
 dist2 = dist[5:10]
+# dist1 = [[x] for x in dist1]
+# dist2 = [[x] for x in dist2]
+dist1 = np.array(dist1).reshape(5, 1)
+dist2 = np.array(dist2).reshape(5, 1)
 
-# 改变列表的结构，dist1现在是5个列表的集合
-# 之后我们会看到 nbumpy 中 reshape() 函数也有同样的作用
-dist1 = [[x] for x in dist1]
-dist2 = [[x] for x in dist2]
 
-# temp_max1 是 dist1 中城市的对应最高温度
 temp_max1 = temp_max[0:5]
-# temp_max2 是 dist2 中城市的对应最高温度
 temp_max2 = temp_max[5:10]
 
-# 我们调用SVR函数，在参数中规定了使用线性的拟合函数
-# 并且把 C 设为1000来尽量拟合数据（因为不需要精确预测不用担心过拟合）
+
+from sklearn.svm import SVR
 svr_lin1 = SVR(kernel='linear', C=1e3)
 svr_lin2 = SVR(kernel='linear', C=1e3)
 
-# 加入数据，进行拟合（这一步可能会跑很久，大概10多分钟，休息一下:) ）
 svr_lin1.fit(dist1, temp_max1)
 svr_lin2.fit(dist2, temp_max2)
 
-# 关于 reshape 函数请看代码后面的详细讨论
 xp1 = np.arange(10,100,10).reshape((9,1))
 xp2 = np.arange(50,400,50).reshape((7,1))
+
 yp1 = svr_lin1.predict(xp1)
 yp2 = svr_lin2.predict(xp2)
+
+ax.plot(xp1, yp1, 'r', xp2, yp2, 'b', dist, temp_max, 'go')
+plt.show()
